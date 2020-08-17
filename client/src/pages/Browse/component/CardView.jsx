bb7ad631e0c6f5566b9be8fded1cc050540376b2
NotBlueBeast <amanssingh007@gmail.com>
2020-08-17 16:40:14 +0530

    cardview swipe working for now

--- client/src/pages/Browse/component/CardView.jsx
+++ client/src/pages/Browse/component/CardView.jsx
@@ -10,24 +10,56 @@ import CardPreviewModal from './CardPreviewModal';
 const CardView = (props) => {
   const [showModel, setshowModel] = useState(false);
   const [modelData, setmodelData] = useState([]);
+  const [slides, setslides] = useState(window.innerWidth > 1000 ? 6 : 3);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      window.location.reload();
+    });
+    return () => {
+      window.removeEventListener('resize', () => {});
+    };
+  }, []);
 
   return (
     <div className="cardview">
       <div className="cardview-title">{props.title}</div>
       <div className="cardview-container">
-        <Swiper style={{ width: '100vw' }} spaceBetween={200} slidesPerView={3}>
-          {props.list.map((item, key) => (
-            <SwiperSlide>
-              <Card
-                key={key}
-                data={item}
-                showModel={showModel}
-                setshowModel={setshowModel}
-                setmodelData={setmodelData}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {slides === 3 ? (
+          <Swiper
+            style={{ width: '100%', zIndex: '1' }}
+            spaceBetween={140}
+            slidesPerView={3}>
+            {props.list.map((item, key) => (
+              <SwiperSlide style={{ width: '100%' }}>
+                <Card
+                  key={key}
+                  data={item}
+                  showModel={showModel}
+                  setshowModel={setshowModel}
+                  setmodelData={setmodelData}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        ) : (
+          <Swiper
+            style={{ width: '100%' }}
+            spaceBetween={200}
+            slidesPerView={6}>
+            {props.list.map((item, key) => (
+              <SwiperSlide style={{ width: '100%' }}>
+                <Card
+                  key={key}
+                  data={item}
+                  showModel={showModel}
+                  setshowModel={setshowModel}
+                  setmodelData={setmodelData}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
       <CardPreviewModal showModel={showModel} modelData={modelData} />
     </div>
